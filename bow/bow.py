f5b7c91e01b5d27a6d898cd3c326d66c7308867c
Timon <19689305+timonvdvelde@users.noreply.github.com>
2018-09-20 14:54:28 +0200

    JSON conversion for word embeddings

--- bow/bow.py
+++ bow/bow.py
@@ -1,5 +1,60 @@
 #!/usr/bin/env python3
 
+import json
+
+# For all reviews:
+#   Load review
+#   review_vector = []
+#   for word in review:
+#     find vector for word
+#     review_vector += vector / nr_words
+#   store review_vector
+
+path_data = '../dataset/train/'
+path_pos = path_data + 'pos/'
+path_neg = path_data + 'neg/'
+
+path_embed = '../embeddings/'
+file_embed_raw = 'glove.twitter.27B.25d.txt'
+file_embed_json = 'glove.twitter.27B.25d.json'
+
+embeddings = None
+
+
+def embed_to_json():
+    """
+    Converts raw embeddings to JSON file and stores it.
+    """
+    table = {}
+
+    with open(path_embed + file_embed_raw) as file:
+        for line in file:
+            values = line.split()
+            float_values = [float(val) for val in values[1:]]
+            table[values[0]] = float_values
+
+    with open(path_embed + file_embed_json, 'w') as outfile:
+        json.dump(table, outfile)
+
+
+def load_embed():
+    """
+    Loads a JSON file with embeddings.
+    """
+    global embeddings
+
+    with open(path_embed + file_embed_json) as file:
+        embeddings = json.load(file)
+
+
 if __name__ == '__main__':
-    print('bow wow wow yippie yo yippie yay')
+    #print('Converting to JSON')
+    #embed_to_json()
+    #print('Great success')
+
+    print('Loading JSON dictionary')
+    load_embed()
+
+    print(embeddings['legaaal'])
+
 
