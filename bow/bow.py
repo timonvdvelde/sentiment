5a16aa0c94857b94a00623e2f5ce4ef5dc626d74
Timon <19689305+timonvdvelde@users.noreply.github.com>
2018-09-22 13:34:03 +0200

    Cleaned things up a bit. Now load review vectors to arrays.

--- bow/bow.py
+++ bow/bow.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
 
-import json
-import numpy as np
-import os
 import sys
-
-# For all reviews:
-#   Load review
-#   review_vector = []
-#   for word in review:
-#     find vector for word
-#     review_vector += vector / nr_words
-#   store review_vector
+from preprocess import *
 
 path_data = '../dataset/train/'
 path_pos = path_data + 'pos/'
@@ -23,105 +13,31 @@ file_embed_json = 'glove.twitter.27B.25d.json'
 
 file_review_vectors = 'review_vectors.json'
 
-embeddings = None
-
-
-def embed_to_json():
-    """
-    Converts raw embeddings to JSON file and stores it.
-    """
-    table = {}
-
-    with open(path_embed + file_embed_raw) as file:
-        for line in file:
-            values = line.split()
-            float_values = [float(val) for val in values[1:]]
-            table[values[0]] = float_values
-
-    store_json(table, path_embed + file_embed_json)
-
-
-def load_embed():
-    """
-    Loads a JSON file with embeddings and converts all the vectors to Numpy
-    arrays for math purposes later on.
-    """
-    global embeddings
-
-    with open(path_embed + file_embed_json) as file:
-        embeddings = json.load(file)
-
-        for key in embeddings.keys():
-            embeddings[key] = np.array(embeddings[key])
-
-
-def review_to_vector(path):
-    """
-    Opens a review file at some path and returns an average of all the word
-    embeddings found in the document.
-    """
-    vector = np.zeros(0)
-    i = 0
-
-    with open(path) as file:
-        for line in file:
-            tokens = line.split(' ')
-
-            for token in tokens:
-                if token in embeddings:
-                    if vector.size == 0:
-                        vector = np.array(embeddings[token])
-                    else:
-                        vector += embeddings[token]
-                    i += 1
-
-    vector /= i
-    return vector.tolist()
-
-
-def all_reviews_to_vectors(path, label, data):
-    """
-    Converts all reviews under a given path to vectors and appends them labeled
-    to existing data.
-    """
-    files = []
-
-    for dirpath, dirnames, filenames in os.walk(path):
-        for filename in filenames:
-            files.append(dirpath + '/' + filename)
-        break
-
-    for file in files:
-        vector = review_to_vector(file)
-        data.append([vector, label])
-
-
-def store_json(data, filename):
-    """
-    Stores data as JSON.
-    """
-    with open(filename, 'w') as file:
-        json.dump(data, file)
-
 
 def preprocess(embed=False):
+    """
+    Loads embeddings to convert the review files into average word vectors. The
+    results are stored in a file.
+    If embed == True, also converts .txt file word embeddings to JSON and stores
+    it.
+    """
     if embed:
         print('Converting to JSON')
-        embed_to_json()
+        embed_to_json(path_embed + file_embed_raw, path_embed + file_embed_json)
         print('Great success')
 
     print('Loading JSON dictionary')
-    load_embed()
+    embeddings = load_embed(path_embed + file_embed_json)
     print('Done')
 
     review_vectors = []
     print('Computing positive review vectors')
-    all_reviews_to_vectors(path_pos, 1, review_vectors)
+    all_reviews_to_vectors(embeddings, path_pos, 1, review_vectors)
     print('Computing negative review vectors')
-    all_reviews_to_vectors(path_neg, 0, review_vectors)
+    all_reviews_to_vectors(embeddings, path_neg, 0, review_vectors)
     print('Done')
 
-    store_json(review_vectors, file_review_vectors)    
+    store_json(review_vectors, file_review_vectors)
 
 
 if __name__ == '__main__':
@@ -129,6 +45,9 @@ if __name__ == '__main__':
         preprocess(embed=True)
     elif sys.argv[1] == '1':
         preprocess(embed=False)
+    elif sys.argv[1] == '2':
+        review_vectors = load_review_vectors(file_review_vectors)
+        print(review_vectors[0])
     else:
         print('Specify what to do')
 
