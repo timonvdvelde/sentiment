5fca48fc0a7b3cdccef5937baf3df5b85b7fa86c
bramvdheuvel <bramjvandenheuvel@gmail.com>
2018-09-24 14:34:10 +0200

    petition to change dataset to data

--- bramlp/train.py
+++ bramlp/train.py
@@ -8,6 +8,8 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 import model as md
 
+# Loading data
+
 # Temporary lol
 v_dim = 25
 hidden_layers = [100]
@@ -27,40 +29,5 @@ if use_cuda:
 optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=moment)
 
 criterion = nn.CrossEntropyLoss()
-
-for epoch in xrange(10):
-    # trainning
-    ave_loss = 0
-    for batch_idx, (x, target) in enumerate(train_loader):
-        optimizer.zero_grad()
-        if use_cuda:
-            x, target = x.cuda(), target.cuda()
-        x, target = Variable(x), Variable(target)
-        out = model(x)
-        loss = criterion(out, target)
-        ave_loss = ave_loss * 0.9 + loss.data[0] * 0.1
-        loss.backward()
-        optimizer.step()
-        if (batch_idx+1) % 100 == 0 or (batch_idx+1) == len(train_loader):
-            print('==>>> epoch: {}, batch index: {}, train loss: {:.6f}'.format(
-                epoch, batch_idx+1, ave_loss))
-    # testing
-    correct_cnt, ave_loss = 0, 0
-    total_cnt = 0
-    for batch_idx, (x, target) in enumerate(test_loader):
-        if use_cuda:
-            x, target = x.cuda(), target.cuda()
-        x, target = Variable(x, volatile=True), Variable(target, volatile=True)
-        out = model(x)
-        loss = criterion(out, target)
-        _, pred_label = torch.max(out.data, 1)
-        total_cnt += x.data.size()[0]
-        correct_cnt += (pred_label == target.data).sum()
-        # smooth average
-        ave_loss = ave_loss * 0.9 + loss.data[0] * 0.1
-
-        if(batch_idx+1) % 100 == 0 or (batch_idx+1) == len(test_loader):
-            print('==>>> epoch: {}, batch index: {}, test loss: {:.6f}, acc: {:.3f}'.format(
-                epoch, batch_idx+1, ave_loss, correct_cnt * 1.0 / total_cnt))
 
 torch.save(model.state_dict(), model.name())
