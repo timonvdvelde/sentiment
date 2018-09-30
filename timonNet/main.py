a5fc526ce42bb62e27a98602b1ee818330a93d5f
timonvdvelde <19689305+timonvdvelde@users.noreply.github.com>
2018-10-01 00:23:18 +0200

    Update main.py

--- timonNet/main.py
+++ timonNet/main.py
@@ -20,14 +20,14 @@ path_test_pos = path_test + 'pos/'
 path_test_neg = path_test + 'neg/'
 
 path_embed = '../embeddings/'
-file_embed_raw = 'glove.twitter.27B.25d.txt'
-file_embed_json = 'glove.twitter.27B.25d.json'
+#file_embed_raw = 'glove.twitter.27B.25d.txt'
+#file_embed_json = 'glove.twitter.27B.25d.json'
 
-#file_embed_raw = 'glove_vectors_unsup_movies_25d_lowercase_preservelines.txt'
-#file_embed_json = 'glove_vectors_unsup_movies_25d_lowercase_preservelines.json'
+file_embed_raw = 'glove_vectors_unsup_movies_25d_lowercase_preservelines.txt'
+file_embed_json = 'glove_vectors_unsup_movies_25d_lowercase_preservelines.json'
 
 dimensions = 25
-batch_size = 50
+batch_size = 4
 params_file = 'params'
 
 
@@ -47,9 +47,9 @@ def get_dataset(embeddings, paths, val=False):
             for filename in filenames:
                 files[-1].append(dirpath + '/' + filename)
                 
-                if sentiment == path_neg or path_test_neg:
+                if sentiment == path_neg or sentiment == path_test_neg:
                     labels[-1].append(0)
-                elif sentiment == path_pos or path_test_pos:
+                elif sentiment == path_pos or sentiment == path_test_pos:
                     labels[-1].append(1)
    
     if val:
@@ -85,18 +85,19 @@ def evaluate(data_loader, net):
     
     for i, (vectors, targets) in enumerate(data_loader):
         logits = net(vectors)
-        loss = F.cross_entropy(logits, targets)
-        corrects = (torch.max(logits, 1)[1].view(targets.size()).data == targets.data).sum()
+        loss = float(F.cross_entropy(logits, targets))
+        corrects = float((torch.max(logits, 1)[1].view(targets.size()).data == targets.data).sum())
         accuracy = 100.0 * corrects / len(vectors)
         
-        avg_loss += loss.data
-        avg_accuracy += accuracy.data
+        avg_loss += loss
+        avg_accuracy += accuracy
     
-    avg_loss /= i
-    avg_accuracy /= i
+    avg_loss /= i+1
+    avg_accuracy /= i+1
     
     print('Loss:', avg_loss)
     print('Accuracy:', avg_accuracy)
+    return avg_accuracy
     
 
 def train(train_loader, validation_loader, net, embeddings):
@@ -105,7 +106,9 @@ def train(train_loader, validation_loader, net, embeddings):
     loader.
     """
     optimizer = Adadelta(net.parameters())
+    print('Validation')
     evaluate(validation_loader, net)
+    prev_best_acc = 0
     
     for i in range(50):
         print('Epoch:', i)
@@ -119,12 +122,15 @@ def train(train_loader, validation_loader, net, embeddings):
             optimizer.step()
 
         print('Validation')
-        evaluate(validation_loader, net)
-        torch.save(net.state_dict(), params_file)
+        acc = evaluate(validation_loader, net)
+        
+        if acc > prev_best_acc:
+            torch.save(net.state_dict(), params_file)
+            prev_best_acc = acc
 
-    if i % 10 == 0:
-        print("Testing")
-        test(embeddings, net)
+        if i % 10 == 0:
+            print("Testing")
+            test(embeddings, net)
 
 
 def collate(items):
@@ -154,12 +160,13 @@ def test(embeddings, net=None):
     """
     Evaluates network on test data.
     """
-    print("Loading data.")
+    print("Loading test data.")
     data = get_dataset(embeddings, (path_test_pos, path_test_neg), val=False)
     test_loader = DataLoader(data,
                              batch_size=batch_size,
                              collate_fn=collate)
-
+    print("Data got.")
+                             
     if not net:
         net = TimonNet(dimensions)
         net.load_state_dict(torch.load(params_file))
@@ -178,25 +185,8 @@ def main():
                               collate_fn=collate)
     val_loader = DataLoader(val_data,
                             batch_size=batch_size,
-                            shuffle=True,
                             collate_fn=collate)
 
-    #indices = list(range(len(data)))
-    #random.shuffle(indices)
-    #train_size = int(0.9 * len(data))
-    #train_indices = indices[:train_size]
-    #train_sampler = SubsetRandomSampler(train_indices)
-    #train_loader = DataLoader(data,
-    #                          batch_size=batch_size,
-    #                          sampler=train_sampler,
-    #                          collate_fn=collate)
-
-    #validation_indices = indices[train_size:]
-    #validation_sampler = SubsetRandomSampler(validation_indices)
-    #validation_loader = DataLoader(data,
-    #                               batch_size=batch_size,
-    #                               sampler=validation_sampler,
-    #                               collate_fn=collate)
     print("Data got.")
 
     net = TimonNet(dimensions)
@@ -207,6 +197,7 @@ if __name__ == '__main__':
     if sys.argv[1] == 'train':
         main()
     elif sys.argv[1] == 'test':
+        print("Loading embeddings.")
         embeddings = preprocess.load_embed(path_embed + file_embed_json)
         test(embeddings)
     else:
