76b0a3508373b3a7e6e0a97bb4b9799c406fdf77
bramvdheuvel <bramjvandenheuvel@gmail.com>
2018-09-30 22:03:52 +0200

    woeieieiie

--- bow/model.py
+++ bow/model.py
@@ -12,18 +12,21 @@ class MLPNet(nn.Module):
         self.n_layers.insert(0, n_input)
         self.n_layers.append(n_output)
 
-        print(self.n_layers)
-        self.layers = []
+        self.layers = nn.ModuleList()
 
         for i in range(len(self.n_layers) - 1):
             self.layers.append(nn.Linear(self.n_layers[i], self.n_layers[i+1]))
-
 
     def forward(self, x):
 
         x = x.view(-1, self.n_layers[0])
         for i in range(len(self.n_layers) - 2):
-            x = nn.relu(self.layers[i](x))
+            x = nn.functional.relu(self.layers[i](x))
 
-        x = nn.Softmax(self.layers[-1](x))
+        # x = nn.Softmax(self.layers[-1](x))
+        # x = nn.functional.softmax(self.layers[-1](x), dim=1)
+        x = self.layers[-1](x)
         return x
+
+    def name(self):
+        return "jemoeder"
--- bow/train.py
+++ bow/train.py
@@ -6,20 +6,25 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 # import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
+
 import model as md
+import preprocess as pre
 
 # Loading data
-reviews, targets = load_review_vectors(file_review_vectors)
-print(reviews.shape(), targets.shape())
+file_review_vectors = 'review_vectors.json'
+inputs, targets = pre.load_review_vectors(file_review_vectors)
+
 # Temporary lol
-v_dim = 25
-hidden_layers = [100]
-classes = 2
+v_dim = np.shape(inputs)[1]
+hidden_layers = []
+classes = np.shape(targets)[1]
 learning_rate = 0.01
 moment = 0.9
 use_cuda = False
+num_epochs = 10
 
-## training
+# training
 model = md.MLPNet(n_input=v_dim,
                   n_hidden=hidden_layers,
                   n_output=classes)
@@ -27,8 +32,36 @@ model = md.MLPNet(n_input=v_dim,
 if use_cuda:
     model = model.cuda()
 
-optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=moment)
-
+optimizer = optim.Adam(model.parameters())
 criterion = nn.CrossEntropyLoss()
+
+for epoch in range(num_epochs):
+    accuracy = 0
+    for enum, i in enumerate(np.random.permutation(range(np.shape(inputs)[0]))):
+        x = torch.from_numpy(inputs[i]).float()
+        y = torch.from_numpy(targets[i]).long().reshape(1,2)
+
+        optimizer.zero_grad()
+
+        prediction = model.forward(x)
+        loss = criterion(prediction, y.max(1)[1])
+        loss.backward()
+        optimizer.step()
+
+        if prediction.max(1)[1] == y.max(1)[1]:
+            accuracy += 1
+
+    # # test for accuracy
+    # accuracy = 0
+    # total = 1000
+    # for i_a in np.random.randint(0, np.shape(inputs)[0], size=total):
+    #     x_a = torch.from_numpy(inputs[i_a]).float()
+    #     y_a = torch.from_numpy(targets[i_a]).long().reshape(1,2)
+    #
+    #     prediction_a = model.forward(x_a)
+    #     if prediction_a.max(1)[1] == y_a.max(1)[1]:
+    #         accuracy += 1
+    print("the accuracy is", accuracy/np.shape(inputs)[0])
+
 
 torch.save(model.state_dict(), model.name())
